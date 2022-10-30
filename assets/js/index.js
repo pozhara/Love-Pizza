78d6820a80ff81afe291bfd1c332d57d45a3b36b
pozhara <poliazhara04@gmail.com>
2022-10-30 14:02:21 +0000

    Style quiz box

---
+++ assets/js/index.js
@@ -0,0 +1,52 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Where was gelato invented?",
+    answer: "Italy",
+    options: [
+      "Spain",
+      "Italy",
+      "Germany",
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does gelato translate to?",
+    answer: "Frozen",
+    options: [
+      "Frozen",
+      "Ice-cream",
+      "Ice",
+    ]
+  },
+    {
+    numb: 3,
+    question: "When was gelato created?",
+    answer: "In 16th century",
+    options: [
+      "In 15th century",
+      "In 16th century",
+      "In 14th century",
+    ]
+  },
+    {
+    numb: 4,
+    question: "What's healthier: gelato or ice-cream?",
+    answer: "Gelato",
+    options: [
+      "Ice-cream",
+      "Gelato",
+      "They're the same",
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which was created first:gelato or ice-crema?",
+    answer: "Gelato",
+    options: [
+      "Ice-cream",
+      "Gelato",
+      "Created in the same century",
+    ]
+  },
+];
